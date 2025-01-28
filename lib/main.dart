73ca32c879216f2e1ff3f814ab86332af8cd9813
Aditya <singhadi437@gmail.com>
2025-01-28 11:05:27 +0530

    inital commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
-// import 'package:http/http.dart' as http; 
-import 'dart:convert';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: (),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Home'),
+        ),
+        body: const Center(
+          child: Text('Welcome to MyApp!'),
+        ),
+      ),
     );
   }
 }
